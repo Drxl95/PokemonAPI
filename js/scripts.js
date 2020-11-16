ca56f0fa51c94d4081efc1fda6824b80aeda5a17
Gutterstack <uebelhorr@gmail.com>
2020-11-16 13:58:41 -0600

    Update scripts.js
    -added pokemon chatacter array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert('Hello world');
+let pokemonList = [
 
-let favoriteFood = 'burgers';
-document.write(favoriteFood);
+    let Charizard = {
+        name = 'Charizard',
+        height = 1.7,
+        types = ['Monster','Dragon']
+    };
+
+    let Pikachu = {
+        name = 'Pikachu',
+        height = 0.4,
+        types = ['Field','Fairy']
+    };
+
+    let Snorlax = {
+        name = 'Snorlax',
+        height = 2.1,
+        types =  ['Monster']
+    };
+];
