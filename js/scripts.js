65be5cf729e33858d89eef7a2c483810841d08d2
Gutterstack <uebelhorr@gmail.com>
2020-11-17 13:11:04 -0600

    Update scripts.js
    -changed object

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 let pokemonList = [
 
-    let Charizard = {
-        name = 'Charizard',
-        height = 1.7,
-        types = ['Monster','Dragon']
-    };
-
-    let Pikachu = {
-        name = 'Pikachu',
-        height = 0.4,
-        types = ['Field','Fairy']
-    };
-
-    let Snorlax = {
-        name = 'Snorlax',
-        height = 2.1,
-        types =  ['Monster']
-    };
-];
+        {
+           name = 'Charizard',
+           height = 1.7,
+           types = ['Monster','Dragon']
+        },
+   
+        {
+           name = 'Pikachu',
+           height = 0.4,
+           types = ['Field','Fairy']
+        },
+   
+        {
+           name = 'Snorlax',
+           height = 2.1,
+           types =  ['Monster']
+        }
+]
