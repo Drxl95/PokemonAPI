d912b7f55ad931e5f31dcbff1c25a887aeda6263
Gutterstack <uebelhorr@gmail.com>
2020-11-25 21:47:45 -0600

    Update scripts.js
    fixed if else statement in loop

--- js/scripts.js
+++ js/scripts.js
@@ -22,18 +22,24 @@ let pokemonList = [
 //Lists the name of the pokemon and their height//
 
 for (let i = 0; i < pokemonList.length; i++) {
-   if (pokemonList[i].name = 'Charizard'){
-      document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
-   }else if (pokemonList[i].name = 'Pikachu'){
-      document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
+   // if (pokemonList[i].name = 'Charizard'){
+   //    document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
+   // }else if (pokemonList[i].name = 'Pikachu'){
+   //    document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
 
-   }else {
-      document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
-   }
+   // }else {
+   //    document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
+   // }
 
    //If the pokemon's height is above 1.5, it will say its a big pokemon//
-   if (pokemonList[i].height = 1.5) {
+   // if (pokemonList[i].height >= 1.5) {
+   //    document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.' + '-Wow, that\'s a big pokemon!');
+   // }
+   if (pokemonList[i].height >= 1.5) {
       document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.' + '-Wow, that\'s a big pokemon!');
+   }
+   else {
+      document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
    }
 }
 
