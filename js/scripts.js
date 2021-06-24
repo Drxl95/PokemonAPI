15aacc9f140e7a9f7c60cfd40a0f2403a4362e73
Drxl95 <uebelhorr@gmail.com>
2021-06-23 21:03:47 -0500

    ajax lesson commits

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,7 @@
 
 let pokemonRepository = (function () {
-   let pokemonList = [
-         {
-            name: 'Charizard',
-            height: 1.7,
-            types: ['Monster',' Dragon']
-         },
-   
-         {              
-            name: 'Pikachu',              
-            height: 0.4,               
-            types: ['Field',' Fairy']              
-         },             
-
-         {              
-            name: 'Snorlax',              
-            height: 2.1,               
-            types:  ['Monster']              
-         }              
-   ];
+   let pokemonList = [];
+   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
 function getAll() {
    return pokemonList;
@@ -39,32 +22,61 @@ function addListItem(pokemon) {
    });
 }
 function showDetails(pokemon) {
-   console.log(pokemon);
+   loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+   });
+   }
+
+function loadList() {
+   return fetch(apiUrl).then(function (response) {
+      return response.json();
+   }).then(function (json) {
+      json.results.forEach(function (item) {
+         let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+         };
+         add(pokemon);
+      });
+   }).catch(function (e) {
+      console.error(e);
+   })
 }
+
+function loadDetails(item) {
+   let url = item.detailsUrl;
+   return fetch(url).then(function (response) {
+      return response.json();
+   }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+   }).catch(function (e) {
+      console.error(e);
+   });
+}
+
 
 return {
    add: add,
    getAll: getAll,
-   addListItem: addListItem
+   addListItem: addListItem,
+   loadList: loadList,
+   loadDetails: loadDetails
 };
 })();
 
 
+pokemonRepository.loadList().then(function() {
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-   pokemonRepository.addListItem(pokemon);
+   pokemonRepository.getAll().forEach(function(pokemon) {
+      pokemonRepository.addListItem(pokemon);
+   });
 });
 
 
 
 
-// for (let i = 0; i < pokemonList.length; i++) {   //Lists the name of the pokemon and their height//
-//    if (pokemonList[i].height >= 1.5) {     //If the pokemon's height is above 1.5, it will say its a big pokemon//
-//       document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.' + '-Wow, that\'s a big pokemon!');
-//    }
-//    else {
-//       document.write(pokemonList[i].name + ' is' + pokemonList[i].height +' meters tall.');
-//    }
-// }
+
 
 
