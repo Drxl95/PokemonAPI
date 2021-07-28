2fe9d77ad72e4a21cbe7949c2d4327706da2ebc8
Drxl95 <uebelhorr@gmail.com>
2021-07-28 10:40:51 -0500

    backtotop

--- js/scripts.js
+++ js/scripts.js
@@ -168,23 +168,41 @@ pokemonRepository.loadList().then(function () {
    });
 });
 
-jQuery(document).ready(function () {
-   var offset = 220;
-   var duration = 500;
-   jQuery(window).scroll(function () {
-      if (jQuery(this).scrollTop() > offset) {
-         jQuery('.back-to-top').fadeIn(duration);
-      } else {
-         jQuery('.back-to-top').fadeOut(duration);
-      }
+$(window).scroll(function () {
+   var height = $(window).scrollTop();
+   if (height > 100) {
+      $('.back-to-top').fadeIn();
+   } else {
+      $('.back-to-top').fadeOut();
+   }
+});
+$(document).ready(function () {
+   $('.back-to-top').click(function (event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
    });
 
-   jQuery('.back-to-top').click(function (event) {
-      event.preventDefault();
-      jQuery('html, body').animate({ scrollTop: 0 }, duration);
-      return false;
-   })
 });
+
+
+// jQuery(document).ready(function () {
+//    var offset = 220;
+//    var duration = 500;
+//    jQuery(window).scroll(function () {
+//       if (jQuery(this).scrollTop() > offset) {
+//          jQuery('.back-to-top').fadeIn(duration);
+//       } else {
+//          jQuery('.back-to-top').fadeOut(duration);
+//       }
+//    });
+
+//    jQuery('.back-to-top').click(function (event) {
+//       event.preventDefault();
+//       jQuery('html, body').animate({ scrollTop: 0 }, duration);
+//       return false;
+//    })
+// });
 
 
 
